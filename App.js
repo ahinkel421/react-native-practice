bfb9146821854b4345a5ebadc3b14af89574c65c
Adam Hinkel <ahinkel421@gmail.com>
2018-07-02 08:21:15 -0400

    file changed

--- App.js
+++ App.js
@@ -18,13 +18,13 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native, Madeleine!</Text>
-        <Text style={styles.instructions}>
-     To get started, edit App.js now!
-             </Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-            </View>
+                  <View style={styles.container}>
+                    <Text style={styles.welcome}>Welcome to React Native, Madeleine!</Text>
+                    <Text style={styles.instructions}>
+                To get started, edit App.js!
+                        </Text>
+                    <Text style={styles.instructions}>{instructions}</Text>
+                        </View>   
     );
   }
 }
